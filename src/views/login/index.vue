585d2f291019cbe9287f26d0f2ac7c1f0e647e36
mesiade <feather746@hotmail.com>
2021-05-07 17:11:28 +0800

    登录页布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,67 @@
 <template>
   <div class="login">
-  12312312312312312login
+  <el-form
+    ref="form"
+    :model="form"
+    label-width="80px"
+    :label-position="'top'">
+    <el-form-item label="手机号">
+      <el-input v-model="form.phone"></el-input>
+    </el-form-item>
+
+    <el-form-item label="密码">
+      <el-input
+      v-model="form.password"
+      type = "password"
+      ></el-input>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="primary"
+      @click="onSubmit"
+      >登录</el-button>
+    </el-form-item>
+  </el-form>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      form: {
+        phone: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('dianji')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.login {
+  height: 100vh;
+  display: flex;
+  // 设置内部元素垂直水平居中
+  justify-content: center;
+  align-items: center;
+
+  .el-form {
+    background-color: #fff;
+    padding: 20px;
+    width: 300px;
+    border-radius: 10px;
+
+    .el-button{
+      width: 100%;
+    }
+  }
+}
 </style>
