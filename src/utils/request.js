d3d3178c79fc9fa16f8bcfb23af1739c4539993f
mesiade <feather746@hotmail.com>
2021-05-08 15:48:31 +0800

    封装登录请求

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,22 +1,19 @@
 import axios from 'axios'
 
 const request = axios.create({
-  timeout: 5000
+//   timeout: 5000
 })
 
-
 function getBaseUrl (url) {
-    if (url.startsWith('/boss')) {
-      return 'http://eduboss.lagou.com'
-    } else {
-      return 'http://edufront.lagou.com'
-    }
+  if (url.startsWith('/boss')) {
+    return 'http://eduboss.lagou.com'
+  } else {
+    return 'http://edufront.lagou.com'
+  }
 }
-
 
 // 创建拦截器
 request.interceptors.request.use(function (config) {
-  
   // 判断config.url的前缀，来进行请求baseURL的设置
   config.baseURL = getBaseUrl(config.url)
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,13 +3,20 @@
   <el-form
     ref="form"
     :model="form"
+    :rules="rules"
     label-width="80px"
     :label-position="'top'">
-    <el-form-item label="手机号">
+    <el-form-item
+      label="手机号"
+      prop="phone"
+      >
       <el-input v-model="form.phone"></el-input>
     </el-form-item>
 
-    <el-form-item label="密码">
+    <el-form-item
+      label="密码"
+      prop="password"
+      >
       <el-input
       v-model="form.password"
       type = "password"
@@ -19,6 +26,7 @@
     <el-form-item>
       <el-button type="primary"
       @click="onSubmit"
+      :loading="isLoginLoading"
       >登录</el-button>
     </el-form-item>
   </el-form>
@@ -27,19 +35,67 @@
 
 <script>
 
+// 引入封装的接口功能组件
+import { login } from '@/services/user'
+
 export default {
   name: 'LoginIndex',
   data () {
     return {
+      // 存储表单数据的独享
       form: {
         phone: '',
         password: ''
-      }
+      },
+      // 用于设置表单校验规则
+      rules: {
+        phone: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { pattern: /^1\d{10}$/, message: '请输入正确的手机号码', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 18, message: '密码长度为6到18位', trigger: 'blur' }
+        ]
+      },
+      // 用于保存加载状态
+      isLoginLoading: false
     }
   },
   methods: {
-    onSubmit () {
-      console.log('dianji')
+    async onSubmit () {
+      // this.$refs.form.validate(valid => {})
+      try {
+        // 1. 设置校验成功后的功能
+        await this.$refs.form.validate()
+
+        // 2. 发送请求
+        this.isLoginLoading = true
+
+        // const { data } = await request({
+        //   method: 'POST',
+        //   url: '/front/user/login',
+        //   // urlencoded
+        //   data: qs.stringify(this.form)
+        // })
+
+        const { data } = await login(this.form)
+
+        this.isLoginLoading = false
+
+        // 3.响应处理
+        if (data.state === 1) {
+          // 成功后跳转到首页
+          this.$router.push({
+            name: 'home'
+          })
+          this.$message.success('登录成功')
+        } else {
+          this.$message.error('登录失败')
+        }
+      } catch (err) {
+        // 设置校验失败后的功能
+      }
     }
   }
 }
