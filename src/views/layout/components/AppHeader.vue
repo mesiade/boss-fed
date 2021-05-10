0b5878000afea4cb0329fded450a837d5ed26b72
mesiade <feather746@hotmail.com>
2021-05-10 09:47:46 +0800

    退出登录

--- src/views/layout/components/AppHeader.vue
+++ src/views/layout/components/AppHeader.vue
@@ -20,7 +20,10 @@
       </span>
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item>{{ userInfo.userName }}</el-dropdown-item>
-        <el-dropdown-item divided>退出</el-dropdown-item>
+        <el-dropdown-item
+        divided
+        @click.native="handleLogout"
+        >退出</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
   </div>
@@ -48,6 +51,29 @@ export default {
     async loadUserInfo () {
       const { data } = await getUserInfo()
       this.userInfo = data.content
+    },
+    // 退出功能
+    handleLogout () {
+      this.$confirm('确认退出吗?', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 1. 清除 store 中的用户信息
+        this.$store.commit('setUser', null)
+        // 2. 跳转登录页
+        this.$router.push('/login')
+        // 3. 弹出提示
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
     }
   }
 }
