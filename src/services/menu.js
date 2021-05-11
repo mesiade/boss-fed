0e743e4c986e2357191745109ff3a7c07f40581f
mesiade <feather746@hotmail.com>
2021-05-11 10:34:25 +0800

    权限管理-添加菜单

---
+++ src/services/menu.js
@@ -0,0 +1,18 @@
+import request from '@/utils/request'
+
+// 获取编辑菜单页面信息
+export const getEditMenuInfo = (id = -1) => {
+  return request({
+    method: 'GET',
+    url: `/boss/menu/getEditMenuInfo?id=${id}`
+  })
+}
+
+// 添加菜单接口
+export const createOrUpdateMenu = data => {
+  return request({
+    method: 'POST',
+    url: '/boss/menu/saveOrUpdate',
+    data
+  })
+}
