87ffa5419e812fbe8ad185ef6ba9f5b0617910c7
mesiade <feather746@hotmail.com>
2021-05-11 16:22:31 +0800

    资源列表整体布局

--- src/views/resource/index.vue
+++ src/views/resource/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="resource">
+  <resource-list></resource-list>
   </div>
 </template>
 
 <script>
 
+import ResourceList from './components/List'
+
 export default {
-  name: 'ResourceIndex'
+  name: 'ResourceIndex',
+  components: {
+    ResourceList
+  }
 }
 </script>
 
